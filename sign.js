5af375d8cf3be90c2118d6b9da38958d6a93e000
tiny9085 <tian9085@126.com>
2021-04-02 17:42:37 +0800

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,19 +1,30 @@
+const cookieName = '网易新闻'
+const cookieKey = 'chavy_cookie_neteasenews'
+const bodyKey = 'chavy_body_neteasenews'
+const chavy = init()
+const cookieVal = JSON.parse(chavy.getdata(cookieKey))
+const bodyVal = chavy.getdata(bodyKey)
 const cookieName = '海底捞'
 const signurlKey = 'signurl_hdl'
 const signheaderKey = 'signheader_hdl'
 const signbodyKey = 'signbody_hdl'
 const hdl = init()
 
+sign()
 let isGetCookie = typeof $request !== 'undefined'
 
+if (isGetCookie) {
    getcookie()
-//    sign()
+} else {
+   sign()
+}
+
 function getcookie() {
   if ($request && $request.method == 'POST') {
       const signurlVal = $request.url
       const signheaderVal = JSON.stringify($request.headers)
       const signbodyVal = $request.body
-  
+
       if (signurlVal) hdl.setdata(signurlVal, signurlKey)
       if (signheaderVal) hdl.setdata(signheaderVal, signheaderKey)
       if (signbodyVal) hdl.setdata(signbodyVal, signbodyKey)
@@ -21,19 +32,48 @@ function getcookie() {
    }
    hdl.done()
 }
-   
+
 function sign() {
+  if (bodyVal) {
+    let url = { url: `https://attendanceserviceapi.pactera.com:8099/LanXin/zh-cn/GPS/AttendanceCheckIn`, headers: cookieVal }
+    url.body = bodyVal
+    chavy.post(url, (error, response, data) => {
+      chavy.log(`${cookieName}, data: ${data}`)
+      let result = JSON.parse(data)
+     console.log(result)
+      /*const title = `${cookieName}`
+      let subTitle = ``
+      let detail = ``
+      if (result.code == 200) {
+        subTitle = '签到结果: 成功'
+        detail = `连签: +${result.data.serialDays}, 金币: ${result.data.awardGoldCoin}, 说明: ${result.msg}`
+      } else if (result.code == 700) {
+        subTitle = '签到结果: 成功 (重复签到)'
+        detail = `说明: ${result.msg}`
+      } else {
+        subTitle = '签到结果: 失败'
+        detail = `编码: ${result.code}, 说明: ${result.msg}`
+      }
+      chavy.msg(title, subTitle, detail)*/
+    })
+  } else {
   const signurlVal = hdl.getdata(signurlKey)
   const signheaderVal = hdl.getdata(signheaderKey)
   const signbodyVal = hdl.getdata(signbodyKey)
-  const url = { url: signurlVal, headers: signheaderVal, body: signbodyVal }
+  const url = { url: signurlVal, headers: JSON.parse(signheaderVal), body: signbodyVal }
   hdl.post(url, (error, response, data) => {
     hdl.log(`${cookieName}, data: ${data}`)
     const title = `${cookieName}`
+    let subTitle = `签到结果: 失败`
+    let detail = `说明: body参数为空`
+    if (isQuanX()) detail += `, QuanX用户请手动抓包 body 参数!`
+    chavy.msg(title, subTitle, detail)
+  }
+
+  chavy.done()
     let subTitle = ''
     let detail = ''
     const result = JSON.parse(data)
-    console.log(result)
     /*if (result.success == true && result.signInfoVO.todaySigned == true) {
       subTitle = `签到结果: 成功`
       detail = `签到奖励: ${result.customInfo.foodNum}火柴, 连签: ${result.signInfoVO.continueDay}天`
@@ -50,40 +90,7 @@ function sign() {
 }
 
 function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-    }
-  }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'POST'
+@@ -79,8 +90,17 @@ function init() {
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
   }
@@ -99,5 +106,6 @@ function init() {
   done = (value = {}) => {
     $done(value)
   }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
   return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, put, done }
 }
